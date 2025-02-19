c496d8ddc9d37567eca0bcddad57437242249170
tristanasbury <tristanasbury07@gmail.com>
2025-02-19 10:04:54 -0500

    Testing hasrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import { CountryRankingPage } from './pages/country-ranking-page'
 import { CountryDetailsPage } from './pages/country-details-page'
 
@@ -9,12 +9,12 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <main className="relative w-full h-svh text-xs lg:text-base">
       <div className="w-full h-full z-10 md:pt-52 md:px-16">
-        <BrowserRouter basename='/country-rankings'>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<CountryRankingPage/>}></Route>
             <Route path="/country/:countryName" element={<CountryDetailsPage/>}></Route>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
 
     </main>
